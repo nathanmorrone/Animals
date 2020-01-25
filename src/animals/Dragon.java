0c12226dac329c2ce30cf88874b6999a738b5a00
nathanmorrone <morronen@sheridancollege.ca>
2020-01-25 01:14:30 -0500

    Input a comment

--- src/animals/Dragon.java
+++ src/animals/Dragon.java
@@ -9,6 +9,7 @@ public class Dragon {
 
     //Deleted all the comments this is a new one O_o
     //hello again
+    //Helloooooooooooooooooooooooooo
     private String name;
     private String specialMove;
     private double health;
